db9822827c10f302a325a0084ffa7d109a4579d8
FortniteGamer696 <120283785+FortniteGamer696@users.noreply.github.com>
2022-12-14 13:39:24 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,15 @@
+repeat wait(1) until game:IsLoaded()
 
+local fortnite = {
+    [9551640993] = "mining%20simulator%202"
+}
+
+
+for i, v in pairs(fortnite) do
+	if string.match(i,game.PlaceId) then
+	    loadstring(game:HttpGet("https://raw.githubusercontent.com/FortniteGamer696/fortnite-hub/main/scripts/"..v..".lua", true))()
+	 else
+	     game.Players.LocalPlayer:Kick("Game not supported.")
+	     wait(9e9)
+	end
+end
