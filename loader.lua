d6f4ecbc13ea09e1aaf60c137bbc3ec2c9293afe
FortniteGamer696 <120283785+FortniteGamer696@users.noreply.github.com>
2023-03-10 23:23:16 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -9,8 +9,5 @@ local fortnite = {
 for i, v in pairs(fortnite) do
 	if string.match(i,game.PlaceId) then
 	    loadstring(game:HttpGet("https://raw.githubusercontent.com/FortniteGamer696/fortnite-hub/main/games/"..v..".lua", true))()
-		break
-	 else
-	     game.Players.LocalPlayer:Kick("Game not supported.")
 	end
 end
